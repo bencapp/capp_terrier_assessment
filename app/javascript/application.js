16d3d814115c7ab519660a21beb607b0c54bda42
Ben Capp <bentucker333@gmail.com>
2023-05-21 08:23:53 -0500

    render popup using jquery

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,33 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "@hotwired/turbo-rails"
-import "controllers"
+import "@hotwired/turbo-rails";
+import "controllers";
+
+import "./jquery";
+
+$(document).ready(onReady);
+
+function onReady() {
+  // create event listeners
+  $(document).on("click", ".schedule-box", popup);
+  $(document).on("click", "#close-popup-btn", hidePopup);
+}
+
+// generate popup
+function popup() {
+  const name = $(this).data("technician");
+  const minutes = 60;
+  console.log();
+  $("body").append(`<div id="popup-bg">
+                    </div>
+                    <div id="popup">
+                      <div>${name} is free for ${minutes} minutes here.</div>
+                      <button id="close-popup-btn">OK</button>
+                    </div>
+  `);
+}
+
+// remove popup
+function hidePopup() {
+  $("#popup-bg").remove();
+  $("#popup").remove();
+}
