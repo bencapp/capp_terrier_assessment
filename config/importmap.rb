356af8a19cffc2480da86b42729ccbc572944157
Ben Capp <bentucker333@gmail.com>
2023-05-22 11:54:40 -0500

    config for heroku

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,6 +1,7 @@
 # Pin npm packages by running ./bin/importmap
 
 pin "application", preload: true
+pin "jquery", to: "jquery.min.js", preload: true
 
 pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
 pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
