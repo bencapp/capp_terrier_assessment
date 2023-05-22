68ddf5a4412e5a1557b5b4cf1aa3545d5725cb97
Ben Capp <bentucker333@gmail.com>
2023-05-22 11:25:23 -0500

    added script src

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,10 +1,11 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+
+//= require jquery
+//= require jquery_ujs
 import "@hotwired/turbo-rails";
 import "controllers";
 
 import "./jquery";
-//= require jquery
-//= require jquery_ujs
 
 $(document).ready(onReady);
 
